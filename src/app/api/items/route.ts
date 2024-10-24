482cca42c8bc867f72e9e3a89fe120ba875b4031
Tomas Romancik <tomasik11lt@gmail.com>
2024-10-24 16:11:05 +0300

    update.

--- src/app/api/items/route.ts
+++ src/app/api/items/route.ts
@@ -1,5 +1,3 @@
-import {NextRequest, NextResponse} from "next/server";
-
 export async function GET() {
   const mock_url = "https://671a3ac2acf9aa94f6a9bd93.mockapi.io/users";
   const resp = await fetch(mock_url, {method: "GET"});
